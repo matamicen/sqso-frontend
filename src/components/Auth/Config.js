0d4d988764a5de311a3ba37b4941777986312079
lisandro.langenauer <lisandro.langenauer@bairesdev.com>
2018-01-19 12:41:47 -0300

    Cambio en configuracion de cognito

--- src/components/Auth/Config.js
+++ src/components/Auth/Config.js
@@ -1,10 +1,10 @@
 export default {
     region: 'us-east-1',
     IdentityPoolId: 'us-east-1:051d18f6-a6bf-4237-af95-33c0f3a45cc1',
-    UserPoolId: 'us-east-1_dqZFpjJEt',
-    ClientId: '5skhq7v5daug3mj2orajp2aa1q',
+    UserPoolId: 'us-east-1_yznBlsoTx',
+    ClientId: '55lhidgnj7jtbo9vn0rrq3c0qa',
     poolData : {
-        UserPoolId:  'us-east-1_dqZFpjJEt', // Your user pool id here
-        ClientId: '5skhq7v5daug3mj2orajp2aa1q'
+        UserPoolId:  'us-east-1_yznBlsoTx', // Your user pool id here
+        ClientId: '55lhidgnj7jtbo9vn0rrq3c0qa'
     }
 }
