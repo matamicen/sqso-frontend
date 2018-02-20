c10712de618815d07f7756bf5fd902e2ba2797fb
lisandrolan <lisandrolan@gmail.com>
2018-02-20 18:04:39 -0300

    mejora de codificacion de render de componentes condicionales

--- src/components/Feed/FeedItem.js
+++ src/components/Feed/FeedItem.js
@@ -1,7 +1,14 @@
 import React from "react";
 import {AudioList} from "../AudioList";
 import {FeedImage} from './FeedImage'
-import {Feed, Icon, Label, Segment, Button, Item} from "semantic-ui-react";
+import {
+    Feed,
+    Icon,
+    Label,
+    Segment,
+    Button,
+    Item
+} from "semantic-ui-react";
 import QSOComments from "../QSOComments";
 import QSOLikeButton from "../QSOLikeButton";
 import {QRAs} from "../QRAs";
@@ -10,22 +17,24 @@ import {bindActionCreators} from 'redux';
 import * as Actions from '../../actions/Actions';
 import PropTypes from 'prop-types';
 
-
 class FeedItem extends React.Component {
     constructor() {
         super();
         this.state = {
             showComment: false
         };
-        this.handleOnComment = this.handleOnComment.bind(this);
-        this.recalculateRowHeight = this.recalculateRowHeight.bind(this);
+        this.handleOnComment = this
+            .handleOnComment
+            .bind(this);
+        this.recalculateRowHeight = this
+            .recalculateRowHeight
+            .bind(this);
     }
 
     shouldComponentUpdate(nextProps) {
         //   console.log("shouldComponentUpdate QSOFEEDITEM" + this.props.qsosFetched);
         return this.props.qsosFetched;
     }
-
 
     handleOnComment() {
         console.log("Comment Pressed");
@@ -35,40 +44,30 @@ class FeedItem extends React.Component {
 
     recalculateRowHeight() {
         console.log("recalculateRowHeight");
-        this.props.recalculateRowHeight(this.props.index);
+        this
+            .props
+            .recalculateRowHeight(this.props.index);
     }
-
 
     render() {
         //    console.log(this.props.qso.idqsos)
-        let image = null;
-        let picList = this.props.qso.media.filter((media) => media.type === "image");        
-        if (picList.length > 0) {
-            image =
 
-                <FeedImage img={picList}              
-                       measure={this.props.measure}/>
-            ;
-        }
+        let picList = this
+            .props
+            .qso
+            .media
+            .filter((media) => media.type === "image");
+        let audioList = this
+            .props
+            .qso
+            .media
+            .filter((media) => media.type === 'audio');
 
+      
+       
 
-        let audioList = this.props.qso.media.filter((media) => media.type === 'audio');
-
-        let audio = null;
-        if (audioList.length > 0) {
-            audio = <AudioList mediaList={audioList}/>;
-        }
-
-        let comment = null;
-        if (this.state.showComment) {
-            comment = <QSOComments
-                qso={this.props.qso}
-                recalculateRowHeight={this.recalculateRowHeight}/>;
-        }
-
-
-                return (
-            <Segment raised >
+        return (
+            <Segment raised>
                 <Feed.Event>
                     <Item.Extra>
                         <Button icon floated='right' size='mini'>
@@ -77,54 +76,60 @@ class FeedItem extends React.Component {
                     </Item.Extra>
                     <Feed.Content>
                         <Feed.Summary>
-                            <QRAs profilepic={this.props.qso.profilepic} qso_owner={this.props.qso.qra}
-                                  qras={this.props.qso.qras}/>
+                            <QRAs
+                                profilepic={this.props.qso.profilepic}
+                                qso_owner={this.props.qso.qra}
+                                qras={this.props.qso.qras}/>
                             <Label>Mode:</Label>{this.props.qso.mode}
                             <Label>Band:</Label>{this.props.qso.band}
-                            <Label>QSO: </Label>{this.props.qso.idqsos}
+                            <Label>QSO:
+                            </Label>{this.props.qso.idqsos}
                         </Feed.Summary>
 
-                        {image}
+                        {
+                            picList.length > 0 && 
+                            <FeedImage img={picList} measure={this.props.measure}/>
+                        }
 
-                        {audio}
+                        {
+                            audioList.length > 0 && 
+                            <AudioList mediaList={audioList}/>
+                        }
 
                         <Feed.Meta>
                             <QSOLikeButton qso={this.props.qso}/>
-                            <Feed.Like onClick={this.handleOnComment.bind(this)}>
+                            <Feed.Like
+                                onClick={this
+                                .handleOnComment
+                                .bind(this)}>
                                 < Icon name='comment outline'/>
                                 Comment
                             </Feed.Like>
                         </Feed.Meta>
                         <Feed.Extra>
-                            {comment}
+                            {
+                                 this.state.showComment &&
+                                 <QSOComments
+                                        qso={this.props.qso}
+                                        recalculateRowHeight={this.recalculateRowHeight}/>
+                                
+                            }
                         </Feed.Extra>
                     </Feed.Content>
                 </Feed.Event>
             </Segment>
         )
-        }
+    }
 }
 
-
-
-const mapStateToProps = (state, qsos) => ({
-    fetchingQSOS: state.default.FetchingQSOS,
-    qsosFetched: state.default.qsosFetched,
-
-});
+const mapStateToProps = (state, qsos) => ({fetchingQSOS: state.default.FetchingQSOS, qsosFetched: state.default.qsosFetched});
 const mapDispatchToProps = (dispatch) => ({
     actions: bindActionCreators(Actions, dispatch)
 });
 
+export default connect(mapStateToProps, mapDispatchToProps, null, {pure: false})(FeedItem);
 
-export default connect(
-mapStateToProps,
-mapDispatchToProps, null, {
-    pure: false
+FeedItem.PropTypes = {
+    qso: PropTypes.object.isRequired,
+    measure: PropTypes.func.isRequired
 }
-)(FeedItem);
-
- FeedItem.PropTypes = {
-     qso : PropTypes.object.isRequired,
-     measure: PropTypes.func.isRequired
- }
--- src/components/Home/PublicNavigation.js
+++ src/components/Home/PublicNavigation.js
@@ -2,7 +2,7 @@ import React from "react";
 import {NavLink, Link} from "react-router-dom";
 import {Menu, Button} from 'semantic-ui-react'
 import NavigationSearch from './NavigationSearch'
-import Divider from "semantic-ui-react/dist/commonjs/elements/Divider/Divider";
+
 const PublicNavigation = () => (
     <Menu fluid attached='top' stackable compact>
         <Menu.Item as={NavLink} to='/'>
