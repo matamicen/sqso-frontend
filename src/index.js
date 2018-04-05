a0f6bf5135d448dd69d191f0523203acda444d9e
LISANDROLAN <LISANDROLAN@GMAIL.COM>
2018-04-05 00:00:27 -0300

    test

--- src/index.js
+++ src/index.js
@@ -1,44 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-//import thunk from 'redux-thunk';
-//import createHistory from 'history/createBrowserHistory'
-import {ConnectedRouter} from 'react-router-redux'
-//import * as defaultState from './reducers/defaultState'
-//import {applyMiddleware, combineReducers, createStore} from 'redux';
-import {Provider} from 'react-redux'
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { Route } from 'react-router-dom';
+import { ConnectedRouter } from 'react-router-redux';
 import store, { history } from './store';
-// import registerServiceWorker from './registerServiceWorker';
-import { unregister } from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker';
 
-// Create a history of your choosing (we're using a browser history in this case)
-// const history = createHistory()
+import App from "./components/App";
 
-// // Build the middleware for intercepting and dispatching navigation actions
-// const middleware = routerMiddleware(history)
-
-// const reducer = combineReducers({
-//     ...defaultState,
-//     router
-// })
-// // Add the reducer to your store on the `router` key
-// // Also apply our middleware for navigating
-// const store = createStore(
-//     reducer,
-//     applyMiddleware(middleware, thunk)
-// )
-
-ReactDOM.render((
-    <div>
-        <Provider store={store}>
-            {/* ConnectedRouter will use the store from Provider automatically */}
-            <ConnectedRouter history={history}>
-                <div>
-                    <App/>
-                </div>
-            </ConnectedRouter>
-        </Provider>
-    </div>
-), document.getElementById('root'))
-// registerServiceWorker()
-unregister();
+render(
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <Route component={App} />
+      </ConnectedRouter>
+    </Provider>,
+    document.getElementById('root')
+  );
+  
+  registerServiceWorker();
