064c0d1cbde11c4d526529844685a153711aa82d
LISANDROLAN <LISANDROLAN@GMAIL.COM>
2018-08-04 16:35:20 -0300

    resolves #74

--- src/components/Home/UserDashboard.js
+++ src/components/Home/UserDashboard.js
@@ -15,19 +15,19 @@ class UserDashboard extends React.PureComponent {
 
         return (
 
-            <Grid>
-                {(navigator.maxTouchPoints === 0) && <Grid.Row columns={3} only='computer'>
-                    <Grid.Column width={3} only="computer">
+            <Grid columns='equal'>
+            {(navigator.maxTouchPoints === 0) && <Grid.Row >
+                    <Grid.Column width={3} >
                         <Advertisement unit='wide skyscraper' test='Wide Skyscraper' centered/>
                     </Grid.Column>
 
-                    <Grid.Column computer={10} tablet={5}>
+                    <Grid.Column width={10}>
 
                         < NewsFeed/>
 
                     </Grid.Column>
 
-                    <Grid.Column width={3} only="computer">
+                    <Grid.Column width={3}>
                         <Advertisement unit='wide skyscraper' test='Wide Skyscraper' centered/>
                     </Grid.Column>
 
