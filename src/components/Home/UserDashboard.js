24006666a62f26479abb767082edb1b7e9753b05
LISANDROLAN <LISANDROLAN@GMAIL.COM>
2018-08-28 17:24:17 -0300

    resolves #88

--- src/components/Home/UserDashboard.js
+++ src/components/Home/UserDashboard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import NewsFeed from "../Feed/NewsFeedContainer";
+import FeedQSO from "../Feed/NewsFeedContainer";
 
 import Grid from 'semantic-ui-react/dist/commonjs/collections/Grid'
 import Advertisement from 'semantic-ui-react/dist/commonjs/views/Advertisement'
@@ -15,33 +15,48 @@ class UserDashboard extends React.PureComponent {
 
         return (
 
-            <Grid columns='equal'>
-            {(navigator.maxTouchPoints === 0) && <Grid.Row >
-                    <Grid.Column width={3} >
-                        <Advertisement unit='wide skyscraper' test='Wide Skyscraper' centered/>
+            <div >
+            <Grid style={{display: 'flex'}} centered>
+                {(navigator.maxTouchPoints === 0) &&
+                    <Grid.Row 
+                        style={{ 
+                                                     
+                            flexWrap: 'nowrap',
+                            justifyContent: 'center'
+                        }}  >
+
+                    <Grid.Column style={{ width: '160px', flex: 'none', marginLeft: 'auto'}}>
+                        <Advertisement unit='wide skyscraper' test='Wide Skyscraper'/>
                     </Grid.Column>
 
-                    <Grid.Column width={10}>
+                    <Grid.Column  
+                        style={{ 
+                        width: '700px', 
+                        marginLeft: '30px', 
+                        // marginRight: '-5px',
+                        flexShrink: '0'}}>
 
-                        < NewsFeed/>
+                        < FeedQSO/>
 
                     </Grid.Column>
 
-                    <Grid.Column width={3}>
-                        <Advertisement unit='wide skyscraper' test='Wide Skyscraper' centered/>
+                    <Grid.Column  style={{ width: '160px', flex: 'none', marginRight: 'auto'}}>
+                        <Advertisement unit='wide skyscraper' test='Wide Skyscraper' />
                     </Grid.Column>
 
                 </Grid.Row>
 }
-                {(navigator.maxTouchPoints > 0) && <Grid.Row columns={1} only='mobile tablet'>
+                {(navigator.maxTouchPoints > 0) && <Grid.Row  >
                     <Grid.Column>
 
-                        < NewsFeed/>
+                        < FeedQSO/>
 
                     </Grid.Column>
+
                 </Grid.Row>
 }
-            </Grid>
+            </Grid>            
+            </div>
 
         );
     }
