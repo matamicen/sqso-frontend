d57148530ebbd31155ec2db81cf96ff21ffdc6dc
LISANDROLAN <LISANDROLAN@GMAIL.COM>
2018-08-29 13:02:52 -0300

    resolves #86

--- src/components/Profile/QRAProfileQsos.js
+++ src/components/Profile/QRAProfileQsos.js
@@ -1,17 +1,26 @@
 import React from 'react'
-import Feed from 'semantic-ui-react/dist/commonjs/views/Feed'
+import NewsFeed from '../Feed/NewsFeedPresentational';
 import FeedItem from "../Feed/FeedItem";
+import Immutable from 'immutable';
+import FeedItemShare from "../Feed/FeedItemShare"
 
-const QRAProfileQsos = (props) => (
-    <div>        
-        {   props.qsos && <Feed>
-            {props
-                .qsos
-                .map((qso, i) => <FeedItem key={i} qso={qso}/>)
+export default class QRAProfileQsos extends React.Component {
+    render() {
+        let qsos = Immutable.List(this.props.qsos);
+        if (this.props.qsos && this.props.qsos.length > 0) {
+            qsos = this.props.qsos.map((qso, i) =>
+               { 
+                    switch (qso.type) {
+                        case "QSO": return <FeedItem key={i} qso={qso}/>;
+                        case "SHARE": return <FeedItemShare key={i} qso={qso}/>;
+                        default: return null; 
+                    }
+        } 
+            )
+        }
+        
+        return (
+            <NewsFeed list={qsos}/>
+        )
+    }
 }
-  
-        </Feed>
-}
-    </div>
-);
-export default QRAProfileQsos
