0240d36e2a2ec1134ef2d2db2a5dfcbb228a7779
LISANDROLAN <LISANDROLAN@GMAIL.COM>
2018-10-13 20:09:35 -0300

    resolves #111

--- src/components/Profile/QRAProfileContainer.js
+++ src/components/Profile/QRAProfileContainer.js
@@ -4,86 +4,117 @@ import QRAProfile from './QRAProfilePresentational'
 import * as Actions from '../../actions/Actions';
 import {withRouter} from "react-router-dom";
 import {connect} from 'react-redux'
+import Dimmer from "semantic-ui-react/dist/commonjs/modules/Dimmer";
+import Loader from "semantic-ui-react/dist/commonjs/elements/Loader";
 
 class QRAProfileContainer extends React.Component {
     constructor() {
         super();
         this.state = {
-           // fetchingData: false,
+            active: true,
             followed: false
         };
-        this.handleButtonClick = this.handleButtonClick.bind(this);
+        this.handleButtonClick = this
+            .handleButtonClick
+            .bind(this);
     }
 
     componentDidMount() {
-        let qraInMemory = this.props.qra ? this.props.qra.qra.qra : "";
-        if (!this.props.fetchingUser && !this.props.userFetched && this.props.token) {            
-            this.props.actions.doFetchUserInfo(this.props.token);
+        let qraInMemory = this.props.qra
+            ? this.props.qra.qra.qra
+            : "";
+        if (!this.props.fetchingUser && !this.props.userFetched && this.props.token) {
+            this
+                .props
+                .actions
+                .doFetchUserInfo(this.props.token);
         }
-        if ((!this.props.fetchingQRA && !this.props.QRAFetched) || (this.props.QRAFetched && ( this.props.match.params.qra !== qraInMemory) )) {
-            this.props.actions.doFetchQRA(this.props.match.params.qra);
+        if ((!this.props.fetchingQRA && !this.props.QRAFetched) || (this.props.QRAFetched && (this.props.match.params.qra !== qraInMemory))) {
+            this
+                .props
+                .actions
+                .doFetchQRA(this.props.match.params.qra);
         }
-        
-        this.setState({followed : this.props.following.some(o => o.qra === this.props.match.params.qra)});
+
+        this.setState({
+            followed: this
+                .props
+                .following
+                .some(o => o.qra === this.props.match.params.qra)
+        });
 
     }
-
-    shouldComponentUpdate(nextProps) {
-             return nextProps.QRAFetched;
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.QRAFetched) {
+            this.setState({active: false})
+        }
     }
-
+    // shouldComponentUpdate(nextProps) {          return nextProps.QRAFetched; }
 
     handleButtonClick() {
-        
-        if (!this.props.token) return null;
-        this.setState((prevState) => { return{followed: !prevState.followed}; });
+
+        if (!this.props.token) 
+            return null;
+        this.setState((prevState) => {
+            return {
+                followed: !prevState.followed
+            };
+        });
         if (!this.state.followed) {
             if (this.props.isAuthenticated) {
-                this.props.actions.doFollowQRA(this.props.token, this.props.match.params.qra);
+                this
+                    .props
+                    .actions
+                    .doFollowQRA(this.props.token, this.props.match.params.qra);
             }
         } else {
-            this.props.actions.doUnfollowQRA(this.props.token, this.props.match.params.qra);
+            this
+                .props
+                .actions
+                .doUnfollowQRA(this.props.token, this.props.match.params.qra);
         }
     }
 
     render() {
-       
-        if (this.props.fetchingQRA || !this.props.QRAFetched) return null;
-        
+
         let qraInfo = null;
-        if (this.props.qra) qraInfo = this.props.qra.qra;
-        return <QRAProfile qraInfo={qraInfo}
-                           qra={this.props.qra}
-                           onClick={this.handleButtonClick}
-                           isAuthenticated={this.props.isAuthenticated}
-                           currentQRA={this.props.currentQRA}
-                           followed={this.state.followed}/>;
+        if (this.props.qra) 
+            qraInfo = this.props.qra.qra;
+        return (
+            <div>
+
+                <Dimmer active={this.state.active} page>
+                    <Loader>Loading</Loader>
+                </Dimmer>
+                {(this.props.QRAFetched) && <QRAProfile
+                    qraInfo={qraInfo}
+                    qra={this.props.qra}
+                    onClick={this.handleButtonClick}
+                    isAuthenticated={this.props.isAuthenticated}
+                    currentQRA={this.props.currentQRA}
+                    followed={this.state.followed}/>
+}
+            </div>
+        )
+
     }
 }
 
-const
-    mapStateToProps = (state, ownProps) => ({
-        //state: state,
-        currentQRA: state.default.userData.qra,
-        isAuthenticated: state.default.userData.isAuthenticated,
-        following: state.default.userData.following,
-        token: state.default.userData.token,
-        fetchingQRA: state.default.FetchingQRA,
-        QRAFetched: state.default.QRAFetched,
-        qra: state.default.qra,
-        fetchingUser: state.default.userData.fetchingUser,
-        userFetched: state.default.userData.userFetched
+const mapStateToProps = (state, ownProps) => ({
+    //state: state,
+    currentQRA: state.default.userData.qra,
+    isAuthenticated: state.default.userData.isAuthenticated,
+    following: state.default.userData.following,
+    token: state.default.userData.token,
+    fetchingQRA: state.default.FetchingQRA,
+    QRAFetched: state.default.QRAFetched,
+    qra: state.default.qra,
+    fetchingUser: state.default.userData.fetchingUser,
+    userFetched: state.default.userData.userFetched
 
-    });
-const
-    mapDispatchToProps = (dispatch) => ({
-        actions: bindActionCreators(Actions, dispatch)
-    });
+});
+const mapDispatchToProps = (dispatch) => ({
+    actions: bindActionCreators(Actions, dispatch)
+});
 
-export default withRouter(connect(
-        mapStateToProps,
-        mapDispatchToProps,
-        null, {
-            pure: false
-        }
-    )(QRAProfileContainer));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps, null, {pure: false})(QRAProfileContainer));
