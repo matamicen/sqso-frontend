83deb4f999b6d2de0044101f3e545b0688209071
LISANDROLAN <LISANDROLAN@GMAIL.COM>
2018-11-18 17:18:30 -0300

    #112

--- server/index.js
+++ server/index.js
@@ -2,9 +2,9 @@
 require('ignore-styles');
 
 // Set up babel to do its thing... env for the latest toys, react-app for CRA
-require('babel-register')({
-  ignore: /\/(build|node_modules)\//,
-  presets: ['env', 'react-app']
+require("@babel/register")({
+  ignore: [/\/(build|node_modules)\//],
+  presets: ['@babel/env', '@babel/react']
 });
 
 // Now that the nonsense is over... load up the server entry point
