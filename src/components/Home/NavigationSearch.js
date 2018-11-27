c37ea078e63ca235a60d6ba7e69d7760ce595d41
LISANDROLAN <LISANDROLAN@GMAIL.COM>
2018-11-27 12:40:57 -0300

    resolves #35

--- src/components/Home/NavigationSearch.js
+++ src/components/Home/NavigationSearch.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react'
-import Select from 'react-select';
-import 'react-select/dist/react-select.css';
+import Async  from 'react-select/lib/Async';
+// import 'react-select/dist/react-select.css';
 import fetch from 'isomorphic-fetch';
 import {Redirect} from "react-router-dom";
-
+import { components } from 'react-select';
+import Avatar from 'react-avatar';
 
 class NavigationSearch extends Component {
     constructor(props) {
@@ -27,8 +28,9 @@ class NavigationSearch extends Component {
         if (input.length > 2) {
             return fetch(`https://bvi2z1683m.execute-api.us-east-1.amazonaws.com/reactWeb/qra-list?qra=${input}`)
                 .then((response) => response.json())
-                .then((json) => {                    
-                    return {options: json.message};
+                .then((json) => {   
+                                
+                    return json.message;
                 });
         }
     }
@@ -38,29 +40,40 @@ class NavigationSearch extends Component {
             this.setState({value: null});
             return <Redirect to={"/" + this.state.value.qra}/>
         }
-        const AsyncComponent =  Select.Async;
+ 
         if (window.innerWidth <= 380){
             custWidth =  "120px" 
+        } else {
+            custWidth =  "320px" 
         }
         return (
             <div style={{width: custWidth}}>
-                <AsyncComponent multi={false}
+                <Async multi={false}
                                 value={this.state.value}
                                 onChange={this.onChange.bind(this)}
                                // onValueClick={this.gotoUser.bind(this)}
                                 valueKey="qra"
                                 labelKey="name"
-                                placeholder={'Search a QRA'}
+                                placeholder={'Search QRA'}
                                 loadOptions={this.getUsers.bind(this)}
                                 autoload={false}
                                 autosize={false}
                                 autoclear={true}
-                               
+                                components={{Option}}
                                 backspaceRemoves={true}/>
 
             </div>
         );
     }
 }
-
+const GRAVATAR_SIZE = 30;
+const Option = props => {
+    const {data} = props;
+    return (
+      <components.Option {...props}>
+        <Avatar round size={GRAVATAR_SIZE} className="avatar" color="#0366d6" name={data.qra} src={data.avatarpic} />
+        <span>{' ' + data.name}</span>
+      </components.Option>
+    );
+  };
 export default NavigationSearch
