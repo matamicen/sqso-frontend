2b9a68eefad4c5e0a6a849f0defe6696236c2974
LISANDROLAN <LISANDROLAN@GMAIL.COM>
2018-11-28 13:04:38 -0300

    resolves #144

--- src/components/Home/NavigationSearch.js
+++ src/components/Home/NavigationSearch.js
@@ -1,79 +1,115 @@
 import React, {Component} from 'react'
-import Async  from 'react-select/lib/Async';
+import Async from 'react-select/lib/Async';
 // import 'react-select/dist/react-select.css';
-import fetch from 'isomorphic-fetch';
+import API from '@aws-amplify/api';
 import {Redirect} from "react-router-dom";
-import { components } from 'react-select';
+import {components} from 'react-select';
 import Avatar from 'react-avatar';
 
-class NavigationSearch extends Component {
+export default class NavigationSearch extends Component {
     constructor(props) {
         super(props)
 
-        this.state = {value: null}
-
-
+        this.state = {
+            value: null
+        }
 
     }
     onChange(value) {
-        this.setState({
-            value: value,
-        });
+        this.setState({value: value});
 
     }
     getUsers(input) {
+        let result = [];
         if (!input) {
             return Promise.resolve({options: []});
         }
         if (input.length > 2) {
-            return fetch(`https://bvi2z1683m.execute-api.us-east-1.amazonaws.com/reactWeb/qra-list?qra=${input}`)
-                .then((response) => response.json())
-                .then((json) => {   
-                                
-                    return json.message;
+            let apiName = 'superqso';
+            let path = '/qra-list?qra=' + input;
+
+            let myInit = {
+                body: {}, // replace this with attributes you need
+                headers: {
+                    // "Authorization": this.props.token
+                } // OPTIONAL
+            }
+            return API
+                .get(apiName, path, myInit)
+                .then(response => {                    
+                    if (response.body.error > 0) {
+                        console.error(response.body.message);
+                        return result;
+                    } else {
+                        result = response.body.message;
+                        return result;
+                    }
+                })
+                .catch(error => {
+                    console.log(error)
+                    return [];
                 });
+
+            // return
+            // fetch(`https://bvi2z1683m.execute-api.us-east-1.amazonaws.com/reactWeb/qra-li
+            // s t?qra=${input}`).then((response) => response.json()).then((json) => {
+            // return json.message; });
         }
     }
     render() {
-        let custWidth; 
+        let custWidth;
         if (this.state.value) {
             this.setState({value: null});
             return <Redirect to={"/" + this.state.value.qra}/>
         }
- 
-        if (window.innerWidth <= 380){
-            custWidth =  "120px" 
+
+        if (window.innerWidth <= 380) {
+            custWidth = "120px"
         } else {
-            custWidth =  "320px" 
+            custWidth = "320px"
         }
         return (
-            <div style={{width: custWidth}}>
-                <Async multi={false}
-                                value={this.state.value}
-                                onChange={this.onChange.bind(this)}
-                               // onValueClick={this.gotoUser.bind(this)}
-                                valueKey="qra"
-                                labelKey="name"
-                                placeholder={'Search QRA'}
-                                loadOptions={this.getUsers.bind(this)}
-                                autoload={false}
-                                autosize={false}
-                                autoclear={true}
-                                components={{Option}}
-                                backspaceRemoves={true}/>
+            <div style={{
+                width: custWidth
+            }}>
+                <Async
+                    multi={false}
+                    value={this.state.value}
+                    onChange={this
+                    .onChange
+                    .bind(this)}
+                    valueKey="qra"
+                    labelKey="name"
+                    placeholder={'Search QRA'}
+                    loadOptions={this
+                    .getUsers
+                    .bind(this)}
+                    autoload={false}
+                    autosize={false}
+                    autoclear={true}
+                    components={{
+                    Option
+                }}
+                    backspaceRemoves={true}/>
 
             </div>
         );
     }
-}
+};
+
 const GRAVATAR_SIZE = 30;
 const Option = props => {
     const {data} = props;
     return (
-      <components.Option {...props}>
-        <Avatar round size={GRAVATAR_SIZE} className="avatar" color="#0366d6" name={data.qra} src={data.avatarpic} />
-        <span>{' ' + data.name}</span>
-      </components.Option>
+        <components.Option {...props}>
+            <Avatar
+                round
+                size={GRAVATAR_SIZE}
+                className="avatar"
+                color="#0366d6"
+                name={data.qra}
+                src={data.avatarpic}/>
+            <span>{' ' + data.name}</span>
+        </components.Option>
     );
-  };
-export default NavigationSearch
+}
