4987542956e93e30beb18104d39f80ae0b4c5b52
LISANDROLAN <LISANDROLAN@GMAIL.COM>
2018-12-11 20:00:08 -0300

    #147

--- src/components/Notifications/Notification.js
+++ src/components/Notifications/Notification.js
@@ -17,41 +17,40 @@ export default class Notification extends React.Component {
         this
             .props
             .doNotificationRead(this.props.notification.idqra_notifications, this.props.token);
-        
+
     }
     formatNotification() {
         let notif = this.props.notification;
         let message;
         switch (this.props.notification.activity_type) {
-            case 1: //Follow               
+            case 1: //Follow
                 return (
                     <List.Description>
                         <Link to={"/" + notif.REF_QRA} onClick={this.handleOnClick}>{notif.message}</Link>
                     </List.Description>
                 );
-            case 10: //new QSO               
+            case 10: //new QSO
                 return (
                     <List.Description>
                         <Link to={"/qso/" + notif.QSO_GUID} onClick={this.handleOnClick}>{notif.message}</Link>
                     </List.Description>
                 );
-            case 12: //add QRA to QSO                
+            case 12: //add QRA to QSO
                 return (
                     <List.Description>
                         <Link to={"/qso/" + notif.QSO_GUID} onClick={this.handleOnClick}>{notif.message}</Link>
                     </List.Description>
                 );
-            case 18: //add QSO Comment                
+            case 18: //add QSO Comment
                 return (
                     <List.Description>
                         <Link to={"/qso/" + notif.QSO_GUID} onClick={this.handleOnClick}>{notif.message}</Link>
                     </List.Description>
                 );
             case 20: //add QSO Link
-                message = notif.QRA + " linked a QSO with another created by " + notif.REF_QRA;
                 return (
                     <List.Description>
-                        <Link to={"/qso/" + notif.QSO_GUID} onClick={this.handleOnClick}>{message}</Link>
+                        <Link to={"/qso/" + notif.QSO_GUID} onClick={this.handleOnClick}>{notif.message}</Link>
                     </List.Description>
                 );
             case 23: //add QSO Like
