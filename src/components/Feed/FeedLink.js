ea1775ff79273c7d26de56ee393de3e6b9106162
LISANDROLAN <LISANDROLAN@GMAIL.COM>
2019-02-02 13:16:22 -0300

    resolves #169

--- src/components/Feed/FeedLink.js
+++ src/components/Feed/FeedLink.js
@@ -11,9 +11,11 @@ import Dimmer from 'semantic-ui-react/dist/commonjs/modules/Dimmer';
 import Button from 'semantic-ui-react/dist/commonjs/elements/Button'
 import Segment from 'semantic-ui-react/dist/commonjs/elements/Segment';
 class FeedLink extends React.Component {
+    
     state = {
         active: false,
-        showModal: false
+        showModal: false, 
+        qso_link: null
 
     }
     onOpenModal() {
@@ -24,31 +26,30 @@ class FeedLink extends React.Component {
             .doFetchQsoLink(this.props.link.GUID_URL);
 
     }
-    static getDerivedStateFromProps(props, state) {    
-        if (props.qso_link) 
-            return {active: false, showModal: true}
+    static getDerivedStateFromProps(props, state) {
+        if (props.qso_link  && !state.qso_link)             
+            return {active: false, showModal: true, qso_link:props.qso_link}
+        
         return null;
     }
 
-    // componentWillReceiveProps(nextProps) {     if (nextProps.qso_link) {
-    // this.setState({active: false})         this.setState({showModal: true}); } }
-    close = () => {       
-console.log("close")
-        this.setState({showModal: false})
-        this.setState({active: false})
+    close = () => {
+        
+        this.setState({qso_link:null, showModal: false, active: false})
+      
         this
-                    .props
-                    .actions
-                    .doClearQsoLink()
+            .props
+            .actions
+            .doClearQsoLink()
     }
     render() {
-        console.log(this.state.showModal)
+        
         return (
             <Fragment>
                 <Dimmer active={this.state.active} page>
                     <Loader>Loading</Loader>
                 </Dimmer>
-                
+
                 <Segment>
                     <Image
                         src={this.props.link.avatarpic}
@@ -57,28 +58,29 @@ console.log("close")
                         style={{
                         width: '35px',
                         height: '35px'
-                    }}/> {this.props.link.qra} created a linked QSO. <Button size='mini' onClick={()=> this.onOpenModal()}>See Details</Button>
-                
-                <Modal
+                    }}/> {this.props.link.qra}
+                    created a linked QSO.
+                    <Button size='mini' onClick={() => this.onOpenModal()}>See Details</Button>
+                    <Modal                    
                     
- 
-                    closeIcon
-                    open={this.state.showModal}
-                    onClose={()=>{
-                    this
-                    .close()}}
-                    
+                    closeIcon                    
+                    open={(this.state.showModal && this.props.qso_link)?true:false}
+                    onClose={this.close.bind(this)}
                     >
 
                     <Modal.Content>
                         <Modal.Description>
 
-                            {this.props.qso_link && <QSOFeedItem key={this.props.link.idqso_rel} qso={this.props.qso_link} type={this.props.qso_link.type}/>
+                            {this.props.qso_link && <QSOFeedItem
+                                key={this.props.link.idqso_rel}
+                                qso={this.props.qso_link}
+                                type={this.props.qso_link.type}/>
 }
                         </Modal.Description>
                     </Modal.Content>
                 </Modal>
                 </Segment>
+                
             </Fragment>
         )
 
