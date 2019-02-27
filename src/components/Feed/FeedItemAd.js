fdb2110968c8a9bd53dad99a625c67ae04aca253
LISANDROLAN <LISANDROLAN@GMAIL.COM>
2019-02-27 13:13:05 -0300

    #166

--- src/components/Feed/FeedItemAd.js
+++ src/components/Feed/FeedItemAd.js
@@ -3,8 +3,8 @@ import React from "react";
 import "../../styles/style.css";
 import Segment from "semantic-ui-react/dist/commonjs/elements/Segment";
 
-
-import {Bling as GPT} from "react-gpt";
+import AD from 'react-google-publisher-tag';
+// import {Bling as GPT} from "react-gpt";
 export default class FeedItemAd extends React.PureComponent {
 
     render() {
@@ -16,10 +16,7 @@ export default class FeedItemAd extends React.PureComponent {
         
         
         <div id="ad-1" style={{textAlign: 'center'}}>
-                <GPT
-                    adUnitPath="/21799560237/Feed/Feed-Item3"
-                    slotSize={[336, 280]}
-                />
+        <AD path="21799560237/Feed/Feed-Item3" dimensions = {[[336, 280]]} />
             </div>            
         </Segment>
         )
