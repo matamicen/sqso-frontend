7037171a8b818a0f766a67c1dd780ba6480403a0
LISANDROLAN <LISANDROLAN@GMAIL.COM>
2019-02-27 13:04:01 -0300

    #166

--- src/components/Feed/NewsFeedPresentational.js
+++ src/components/Feed/NewsFeedPresentational.js
@@ -19,7 +19,7 @@ export default class NewsFeed extends React.Component {
             loadedRowsMap: {},
             scrollToIndex: undefined,
             loadingRowCount: 0,
-            overscanRowCount: 10,
+            overscanRowCount: 20,
             list: list,
             randomScrollToIndex: null,
             rowCount: list.length
