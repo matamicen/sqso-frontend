d38be7e3addee1101f1ba206202e460a8e25218c
unknown <lisandrolan@gmail.com>
2019-10-16 16:36:45 -0300

    #203

--- src/components/Feed/QSOShareButtons.js
+++ src/components/Feed/QSOShareButtons.js
@@ -8,42 +8,49 @@ import {
 import Icon from "semantic-ui-react/dist/commonjs/elements/Icon";
 
 import Dropdown from "semantic-ui-react/dist/commonjs/modules/Dropdown";
-import Button from "semantic-ui-react/dist/commonjs/elements/Button";
 
 const QSOShareButtons = ({ idqso }) => {
   return (
-    <Button icon>
-      <Icon name="share alternate" />
+    // <Button icon as="div">
+    //   <Icon name="share alternate" />
 
-      <Dropdown upward>
-        <Dropdown.Menu>
-          <Dropdown.Item>
-            <WhatsappShareButton
-              title="CheckOut this QSO"
-              url={window.location.origin + "/qso/" + idqso}
-            >
-              <Icon name="whatsapp" />
-            </WhatsappShareButton>
-          </Dropdown.Item>
-          <Dropdown.Item>
-            <FacebookShareButton
-              quote="CheckOut this QSO"
-              url={window.location.origin + "/qso/" + idqso}
-            >
-              <Icon name="facebook" />
-            </FacebookShareButton>
-          </Dropdown.Item>
-          <Dropdown.Item>
-            <TwitterShareButton
-              title="CheckOut this QSO"
-              url={window.location.origin + "/qso/" + idqso}
-            >
-              <Icon name="twitter" />
-            </TwitterShareButton>
-          </Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
-    </Button>
+    <Dropdown
+      // text="Filter Posts"
+      icon="share alternate"
+      floating
+      // labeled
+      button
+      className="icon"
+      style={{ textAlign: "center" }}
+    >
+      <Dropdown.Menu>
+        <Dropdown.Item style={{ textAlign: "center" }}>
+          <WhatsappShareButton
+            title="CheckOut this QSO"
+            url={window.location.origin + "/qso/" + idqso}
+          >
+            <Icon name="whatsapp" />
+          </WhatsappShareButton>
+        </Dropdown.Item>
+        <Dropdown.Item style={{ textAlign: "center" }}>
+          <FacebookShareButton
+            quote="CheckOut this QSO"
+            url={window.location.origin + "/qso/" + idqso}
+          >
+            <Icon name="facebook" />
+          </FacebookShareButton>
+        </Dropdown.Item>
+        <Dropdown.Item style={{ textAlign: "center" }}>
+          <TwitterShareButton
+            title="CheckOut this QSO"
+            url={window.location.origin + "/qso/" + idqso}
+          >
+            <Icon name="twitter" />
+          </TwitterShareButton>
+        </Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+    // </Button>
   );
 };
 
