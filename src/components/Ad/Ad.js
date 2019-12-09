af2663da7b9eb5aee498f5027da5ef4b1bcb5745
unknown <lisandrolan@gmail.com>
2019-12-09 13:08:34 -0300

    resolves #167

--- src/components/Ad/Ad.js
+++ src/components/Ad/Ad.js
@@ -1,62 +1,62 @@
-// import React from "react";
-// import PropTypes from "prop-types";
-// import { Waypoint } from "react-waypoint";
+import React from "react";
+import PropTypes from "prop-types";
+import { Waypoint } from "react-waypoint";
 
-// let instance = 0;
-// class Ads extends React.Component {
-//   static propTypes = {
-//     id: PropTypes.string,
-//     width: PropTypes.number.isRequired,
-//     height: PropTypes.number.isRequired,
-//     adslot: PropTypes.string
-//   };
+let instance = 0;
+class Ads extends React.Component {
+  static propTypes = {
+    id: PropTypes.string,
+    width: PropTypes.number.isRequired,
+    height: PropTypes.number.isRequired,
+    adslot: PropTypes.string
+  };
 
-//   constructor(props) {
-//     super(props);
-//     this.__id = ++instance;
-//     this.displayAd = this.displayAd.bind(this);
-//   }
+  constructor(props) {
+    super(props);
+    this.__id = ++instance;
+    this.displayAd = this.displayAd.bind(this);
+  }
 
-//   get id() {
-//     return "div-ads-instance-" + (this.props.id || this.__id);
-//   }
+  get id() {
+    return "div-ads-instance-" + (this.props.id || this.__id);
+  }
 
-//   async componentDidMount() {
-//     //   console.log(this.id)
-//     /*   //Comentado Adsense
-//     if (!this.props.displayOnly)
-//        await window.googletag.cmd.push(() => {
-//         window.googletag
-//           .defineSlot(
-//             this.props.adslot,
-//             [this.props.width, this.props.height],
-//             this.id
-//           )
-//           .addService(window.googletag.pubads());
-//       }); */
-//     //Comentado Adsense
-//   }
-//   render() {
-//     return (
-//       <div
-//         style={{
-//           width: this.props.width,
-//           height: this.props.height,
-//           margin: "0 auto"
-//         }}
-//       >
-//         <Waypoint onEnter={this.displayAd} />
+  async componentDidMount() {
+    //   console.log(this.id)
+    /*   //Comentado Adsense
+    if (!this.props.displayOnly)
+       await window.googletag.cmd.push(() => {
+        window.googletag
+          .defineSlot(
+            this.props.adslot,
+            [this.props.width, this.props.height],
+            this.id
+          )
+          .addService(window.googletag.pubads());
+      }); */
+    //Comentado Adsense
+  }
+  render() {
+    return (
+      <div
+        style={{
+          width: this.props.width,
+          height: this.props.height,
+          margin: "0 auto"
+        }}
+      >
+        <Waypoint onEnter={this.displayAd} />
 
-//         <div id={this.id} />
-//       </div>
-//     );
-//   }
-//   displayAd() {
-//     // const id = this.id;
-//     // window.googletag.cmd.push(function() {
-//     //   window.googletag.display(id);
-//     // });
-//   }
-// }
+        <div id={this.id} />
+      </div>
+    );
+  }
+  displayAd() {
+    // const id = this.id;
+    // window.googletag.cmd.push(function() {
+    //   window.googletag.display(id);
+    // });
+  }
+}
 
-// export default Ads;
+export default Ads;
