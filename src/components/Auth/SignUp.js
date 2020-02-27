b9f786bb26a426bbbf90d7c20e82ce101c073f90
unknown <lisandrolan@gmail.com>
2020-02-27 12:58:01 -0300

    resolves #304

--- src/components/Auth/SignUp.js
+++ src/components/Auth/SignUp.js
@@ -203,8 +203,9 @@ class SignUp extends React.Component {
         .oneOf([Yup.ref("password"), null], "Passwords must match"),
       qra: Yup.string("Enter your Callsign")
         .required("Callsign is required")
+        .matches(/^[a-zA-Z0-9]+$/, "Do not include / or any special symbols")
         .min(3, "Callsign is too short")
-        .max(10, "Callsign is too long"),
+        .max(7, "Callsign is too long"),
       birthDate: Yup.date()
         .required("Enter your Date of Birth")
         .min(new Date(1900, 0, 1))
