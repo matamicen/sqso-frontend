cde6adb233365948b98e88629d6a8397de6a9768
unknown <lisandrolan@gmail.com>
2020-04-13 17:53:57 -0300

    resolves #353

--- src/components/Feed/FeedItemQSO.js
+++ src/components/Feed/FeedItemQSO.js
@@ -110,10 +110,10 @@ class FeedItemQSO extends React.Component {
               <b>Band: </b>
               {this.props.qso.band}
             </div>
-            {/* <div>
+            <div>
               <b>RST: </b>
-              {this.props.qso.rst}
-            </div> */}
+              {this.props.qso.rst ? this.props.qso.rst : "59"}
+            </div>
             <div>
               <b>Date: </b>
               {date.toLocaleDateString("EN-US", { month: "short" }) +
