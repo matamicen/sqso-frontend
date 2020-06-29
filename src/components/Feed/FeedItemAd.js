a9fbcc21262a3245e45530428820b7cbdcf97749
unknown <lisandrolan@gmail.com>
2020-06-29 12:17:29 -0300

    habilitar publicidad en feed

--- src/components/Feed/FeedItemAd.js
+++ src/components/Feed/FeedItemAd.js
@@ -1,14 +1,19 @@
-import React from "react";
-
-import "../../styles/style.css";
-import Segment from "semantic-ui-react/dist/commonjs/elements/Segment";
-import Ad from "../Ad/Ad";
+import React from 'react';
+import Segment from 'semantic-ui-react/dist/commonjs/elements/Segment';
+import '../../styles/style.css';
+import Ad from '../Ad/Ad';
 
 export default class FeedItemAd extends React.PureComponent {
   render() {
     return (
       <Segment raised secondary>
-        <Ad adslot="/21799560237/Feed/Feed-Item3" width={336} height={280} />
+        <Ad
+          adslot="/22031658057/Home/home_feed"
+          width={336}
+          height={280}
+          id="div-ads-instance-home-feed"
+          displayOnly={true}
+        />
       </Segment>
     );
   }
