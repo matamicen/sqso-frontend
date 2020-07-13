bf50c059ec86101aac938a73eca8444caa3acfee
unknown <lisandrolan@gmail.com>
2020-07-13 14:24:29 -0300

    actualizar likes de repost en post original

--- src/components/Feed/QSOLikeButton.js
+++ src/components/Feed/QSOLikeButton.js
@@ -104,7 +104,7 @@ class QSOLikeButton extends React.Component {
     let path = '/qso-like';
     let myInit = {
       body: {
-        qso: this.props.qso.idqsos
+        qso: this.props.qso.idqso_shared ? this.props.qso.idqso_shared: this.props.qso.idqsos
       }, // replace this with attributes you need
       headers: {
         Authorization: this.props.token
@@ -140,6 +140,7 @@ class QSOLikeButton extends React.Component {
   }
 
   doUnLike() {
+
     let apiName = 'superqso';
     let path = '/qso-like';
     let myInit = {
