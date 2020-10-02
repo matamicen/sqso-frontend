43da63f4da12d3297b2e079f33e07d0d4851087f
unknown <lisandrolan@gmail.com>
2020-10-02 12:15:34 -0300

    video responsive

--- src/components/Feed/FeedVideoList.js
+++ src/components/Feed/FeedVideoList.js
@@ -1,20 +1,23 @@
-import React from "react";
-import Item from "semantic-ui-react/dist/commonjs/views/Item";
-import "../../styles/style.css";
-import FeedVideo from "./FeedVideo";
+import React from 'react';
+import Item from 'semantic-ui-react/dist/commonjs/views/Item';
+import '../../styles/style.css';
+import { FeedVideo } from './FeedVideo';
 
 class FeedVideoList extends React.Component {
   render() {
     if (this.props.mediaList.length > 0) {
       return (
-        <Item.Group style={{ margin: "0px" }}>
+        <Item.Group style={{ margin: '0px' }}>
           {this.props.mediaList.map((m, i) => (
-            <Item key={i} >
+            <Item key={i}>
               <FeedVideo
                 key={i}
                 index={i}
                 media={m}
-                measure={this.props.measure}
+                measure={() => {
+                  console.log('measure');
+                  this.props.measure();
+                }}
                 qso_owner={this.props.qso_owner}
                 recalculateRowHeight={this.props.recalculateRowHeight}
               />
@@ -28,5 +31,4 @@ class FeedVideoList extends React.Component {
   }
 }
 
-
-export default (FeedVideoList);
+export default FeedVideoList;
